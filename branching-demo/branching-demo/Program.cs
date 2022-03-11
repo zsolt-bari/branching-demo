9e0c88dbe5b282f351072099109c8c1181b34672
Zsolt Bari <zsolt.bari@gsgroup.hu>
2022-03-11 04:57:11 +0100

    added console writeline

--- branching-demo/branching-demo/Program.cs
+++ branching-demo/branching-demo/Program.cs
@@ -7,6 +7,10 @@ namespace branching_demo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
 
 
 
