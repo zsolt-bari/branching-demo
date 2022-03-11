271633505b5709c6ce76ef60852236e8e014b756
hallgato <hallgato@LABOR210-25>
2022-03-11 07:46:38 +0100

    console writeline

--- branching-demo/branching-demo/Program.cs
+++ branching-demo/branching-demo/Program.cs
@@ -11,6 +11,7 @@ namespace branching_demo
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
 
 
 
