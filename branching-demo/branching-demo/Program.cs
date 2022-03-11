10e80846cce620c37088f3a912fc2dc1a07748ab
Zsolt Bari <zsolt.bari@gsgroup.hu>
2022-03-11 04:40:50 +0100

    setup

---
+++ branching-demo/branching-demo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace branching_demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+
+        }
+    }
+}
